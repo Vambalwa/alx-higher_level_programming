16ed5a021959d0f53eee129f2fec5825b23492a1
Vambalwa <velmavutagwa@gmail.com>
2023-01-04 22:10:05 +0300

    fixed some errors

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
+# 0_add.py
 
 if __name__ == "__main__":
-    """
-    Prints the result of the addition between two numbers
-    """
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
