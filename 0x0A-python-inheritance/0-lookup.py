b9ae37e9f932ee3dc5e5aceb6f2964a46bda83db
Vambalwa <velmavutagwa@gmail.com>
2023-01-10 00:05:00 +0300

    tasks and tests

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 0-lookup.py
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+
